b39a8b4d2d92b68fdb055c5e5dc98eaf4c369260
Tomato Suzuo <tz.cytpm@gmail.com>
2025-09-26 23:18:46 +0700

    Update build-desktop-mac.sh

--- scripts/ci/build-desktop-mac.sh
+++ scripts/ci/build-desktop-mac.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-trap "rm apps/multiplatform/local.properties 2> /dev/null || true; rm local.properties 2> /dev/null || true; rm /tmp/simplex.keychain" EXIT
+# trap "rm apps/multiplatform/local.properties 2> /dev/null || true; rm local.properties 2> /dev/null || true; rm /tmp/simplex.keychain" EXIT
 # echo "desktop.mac.signing.identity=Developer ID Application: SimpleX Chat Ltd (5NN7GUYB6T)" >> apps/multiplatform/local.properties
 # echo "desktop.mac.signing.keychain=/tmp/simplex.keychain" >> apps/multiplatform/local.properties
 # echo "desktop.mac.notarization.apple_id=$APPLE_SIMPLEX_NOTARIZATION_APPLE_ID" >> apps/multiplatform/local.properties
